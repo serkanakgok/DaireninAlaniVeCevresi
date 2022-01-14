af0131481cefa1f3ac3dec29f89e155fd45a9c67
serkanakgok <serkan.akgok@hotmail.com>
2022-01-15 01:27:15 +0300

    send from intellij

---
+++ src/DaireDilimi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DaireDilimi {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int r,a;
+        Scanner data = new Scanner(System.in);
+
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        r = data.nextInt();
+
+        System.out.print("Dairenin mrekez açısını giriniz: ");
+        a = data.nextInt();
+
+        double alan = (pi*(r*r)*a)/360;
+        System.out.println(r>0&&a>0 ? "Dairenin alanı: "+alan : "\nBiraz matematik çalış");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int r;
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        Scanner data = new Scanner(System.in);
+        r=data.nextInt();
+
+        double cevre = 2 * pi * r;
+        double alan = pi * r * r;
+        System.out.println(r>0 ? "Dairenin alanı: "+alan+"\nDairenin çevresi: "+cevre : "\nBiraz matematik çalış");
+    }
+}
